acc7edc68f74a9c58d9467f99132706b2eddd1aa
sujal-GITHUB <sujal21102004@gmail.com>
2025-06-24 11:51:38 +0530

    folder structure simplified

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is a basic React TypeScript app.</p>
+    </div>
+  );
+};
+
+export default App;
