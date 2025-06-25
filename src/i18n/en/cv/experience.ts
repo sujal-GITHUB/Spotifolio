b716234f715195b4b710a0d33dee116b777ddde5
sujal-GITHUB <sujal21102004@gmail.com>
2025-06-25 14:12:28 +0530

    made langugae folder

---
+++ src/i18n/en/cv/experience.ts
@@ -0,0 +1,16 @@
+const GURUKUL = {
+  GURUKUL_JOB_NAME: 'Web Developer - Front End Specialist',
+  GURUKUL_JOB_LENGTH: 'Remote, Jun 2024 - Jul 2024',
+  GURUKUL_JOB_DESCRIPTION: `Engineered a responsive web application using React.js and Node.js that achieved 300+ daily active users and a 95% mobile responsiveness score while implementing WCAG standards.\nInitiated code splitting and lazy loading techniques to reduce initial load time by 40%, improving user retention and search engine rankings.`,
+};
+
+const GRAPPLTECH = {
+  GRAPPLTECH_JOB_NAME: 'Software Development Engineer Intern',
+  GRAPPLTECH_JOB_LENGTH: 'Remote, May 2024 - Jun 2024',
+  GRAPPLTECH_JOB_DESCRIPTION: `Developed and implemented a responsive Hero section using React.js and Tailwind CSS, improving site engagement by 25% and reducing bounce rate by 15%.\nLeveraged performance analysis tools to identify and fix rendering bottlenecks, resulting in 30% faster page load times and improved Core Web Vitals.`,
+};
+
+export const EXPERIENCE = {
+  ...GURUKUL,
+  ...GRAPPLTECH,
+};
---
+++ src/i18n/en/order.ts
@@ -0,0 +1,26 @@
+export const order = {
+  ALL: 'All',
+
+  // Experience
+  JOB: 'Jobs',
+
+  // Publications
+  COURSE: 'Courses',
+  EXAM: 'Exams',
+  DEGREE: 'Degrees',
+
+  // Projects
+  WEB: 'Websites',
+  MOBILE: 'Mobile Apps',
+  PERSONAL_PROJECTS: 'Personal Projects',
+
+  // Certificates
+  THESIS: 'Thesis',
+  JOURNAL: 'Journal',
+  CONFERENCE: 'Conference',
+
+  // Skills
+  CURRENT: 'Currently using',
+  PAST_FREQUENTLY: 'I used in the past',
+  PAST_OCCASIONALLY: 'I know a few things about',
+};
