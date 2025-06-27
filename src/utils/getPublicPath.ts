8a660be49f745585b493d15187f9fa3de52a0d2c
sujal-GITHUB <sujal21102004@gmail.com>
2025-06-27 11:14:17 +0530

    import paths fixed

---
+++ src/utils/getPublicPath.ts
@@ -0,0 +1,16 @@
+const getPath = (path: string) => `${process.env.PUBLIC_URL}/${path}`;
+
+export const getSongPath = (song: string) => getPath(`/songs/${song}`);
+
+export const getAlbumPath = (album: string) =>
+  getPath(`images/albums/${album}`);
+
+export const getSocialNetworkPath = (socialNetwork: string) =>
+  getPath(`images/social/${socialNetwork}`);
+
+export const getProjectPath = (project: string) => getPath(`images/projects/${project}`);
+
+export const getExperiencePath = (experience: string) => getPath(`images/experience/${experience}`);
+
+export const getEducationPath = (education: string) => getPath(`images/education/${education}`);
+
