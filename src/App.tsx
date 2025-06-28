7185e61cdc10c83ca12474580b71a85a737dae46
sujal-GITHUB <sujal21102004@gmail.com>
2025-06-28 14:22:50 +0530

    app running gracefully

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,84 @@
-// src/App.tsx
+import './styles/App.scss';
 
-import React from 'react';
+// Utils
+import i18next from 'i18next';
+import { Suspense, lazy, useEffect, useRef } from 'react';
 
-const App: React.FC = () => {
+// Components
+import { ConfigProvider } from 'antd';
+import { AppLayout } from './components/Layout';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+import { libraryActions } from './store/slices/library';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store, useAppSelector } from './store/store';
+import { SearchPage } from './pages/Search';
+
+// Pages
+const Home = lazy(() => import('./pages/Home'));
+const Page404 = lazy(() => import('./pages/404'));
+const Profile = lazy(() => import('./pages/Profile'));
+const PlaylistView = lazy(() => import('./pages/Playlist'));
+
+window.addEventListener('resize', () => {
+  const vh = window.innerWidth;
+  if (vh < 950) {
+    store.dispatch(libraryActions.collapseLibrary());
+  }
+});
+
+const RootComponent = () => {
+  const container = useRef<HTMLDivElement | null>(null);
+  const language = useAppSelector((state) => state.language.language);
+
+  useEffect(() => {
+    document.documentElement.setAttribute('lang', language);
+    i18next.changeLanguage(language);
+  }, [language]);
+
+  const routes = [
+    { path: '', element: <Home /> },
+    { path: '/profile', element: <Profile /> },
+    { path: '/search/:search', element: <SearchPage /> },
+    { path: '/playlist/:playlistId', element: <PlaylistView container={container} /> },
+    { path: '*', element: <Page404 /> },
+  ];
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <p>This is a basic React TypeScript app.</p>
-    </div>
+    <>
+      <Router basename='portfolio'>
+        <AppLayout>
+          <div className='Main-section' ref={container}>
+            <div style={{ minHeight: 'calc(100vh - 230px)', width: '100%' }}>
+              <Routes>
+                {routes.map((route) => (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    element={<Suspense>{route.element}</Suspense>}
+                  />
+                ))}
+              </Routes>
+            </div>
+          </div>
+        </AppLayout>
+      </Router>
+    </>
   );
 };
+
+function App() {
+  return (
+    <ConfigProvider theme={{ token: { fontFamily: 'SpotifyMixUI' } }}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <RootComponent />
+        </PersistGate>
+      </Provider>
+    </ConfigProvider>
+  );
+}
 
 export default App;
