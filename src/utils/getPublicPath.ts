8664510d176223e616da02551524e29779d0ba95
sujal-GITHUB <sujal21102004@gmail.com>
2025-06-28 16:17:30 +0530

    all deploy problem fixed

--- src/utils/getPublicPath.ts
+++ src/utils/getPublicPath.ts
@@ -1,19 +1,9 @@
-const getPath = (path: string) => {
-  const base = process.env.PUBLIC_URL?.replace(/\/$/, '') ?? '';
-  return `${base}${path}`;
-};
+export const getSongPath = (song: string) => `/songs/${song}`;
 
-export const getSongPath = (song: string) => getPath(`/songs/${song}`);
+const getPath = (path: string) => `${process.env.PUBLIC_URL}/${path}`;
 
-export const getAlbumPath = (album: string) =>
-  getPath(`images/albums/${album}`);
-
-export const getSocialNetworkPath = (socialNetwork: string) =>
-  getPath(`images/social/${socialNetwork}`);
-
+export const getAlbumPath = (album: string) => getPath(`images/albums/${album}`);
+export const getSocialNetworkPath = (socialNetwork: string) => getPath(`images/social/${socialNetwork}`);
 export const getProjectPath = (project: string) => getPath(`images/projects/${project}`);
-
 export const getExperiencePath = (experience: string) => getPath(`images/experience/${experience}`);
-
 export const getEducationPath = (education: string) => getPath(`images/education/${education}`);
-
